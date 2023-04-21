7f120cbdb314fe39eb9581cac7bc4e053f860333
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-04-21 20:00:17 +0000

    Work cont'd

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,31 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+favorite_foods = ["tacos","burritos","nachos"]
+# puts favorite_foods.inspect
+
+numbers = [4, 8, 15, 16, 23, 42]
+# puts numbers.inspect
+
+mixed_array = ["tacos", 12, true]
+# puts mixed_array.inspect
+
+shopping_lists = [["milk","eggs","toilet paper"],["soap","shampoo"]]
+# puts shopping_lists.inspect
 
 # Accessing the array
+# puts favorite_foods[0]
+# puts favorite_foods[-1]
+puts shopping_lists[1][0]
 
 # Add to the array
+favorite_foods.push("more tacos")
+puts favorite_foods.inspect
+
+favorite_foods = favorite_foods + ["fries", "ramen"]
+puts favorite_foods.inspect
+puts favorite_foods.count
+
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- labs/2-conditionals.rb
+++ labs/2-conditionals.rb
@@ -30,3 +30,11 @@ puts die2
 # 3. create a variable for the total and write it to the screen
 total = die1 + die2
 puts "The total is: #{total}"
+
+if total==7 or total==11
+    puts "YOU WIN!"
+elsif total==2 or total==3 or total==12
+    puts "YOU LOSE!"
+else 
+    puts "THE POINT IS #{total}"
+end
