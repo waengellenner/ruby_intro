363f3699627011b81b96c92763cf550ce87ff623
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-04-21 21:22:40 +0000

    Work cont'd (finalize)

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,29 @@
 #   puts "tacos!"
 # end
 
+# index = 0
+# loop do
+#     if index == 5
+#         break
+#     end
+
+#     puts "tacos!"
+#     index = index + 1
+# end
+
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+ tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# index = 0
+# loop do
+#     if index == tacos.count
+#         break
+#     end
+
+#     puts "#{tacos[index]} tacos!"
+#     index = index +1
+# end
+
+for taco in tacos
+    puts "#{taco} tacos!"
+end
