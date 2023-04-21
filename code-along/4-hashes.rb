9b615c8c8a1bbb6156cc417e9434d1205a7b1a7c
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-04-21 20:37:30 +0000

    Work cont'd

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,19 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+me = {
+    "name" => "Brian",
+    "location" => "Chicago",
+    "timeline" => [
+       {"status" => "ENTR-451 at Kellogg", "posted" => "07:30am"},
+       {"status" => "Eating tacos", "posted" => "06:30am"}
+    ]
+}
 
 # Accessing data from the hash
+# puts me["name"]
+# puts me["location"]
+puts me["timeline"][0]
+puts me["timeline"][0]["status"]
 
 # More Complex Hashes
